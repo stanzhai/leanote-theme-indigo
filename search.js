dea5ca8c1ef056e3ef9c1308c98f0564955b3712
Stan Zhai <zhaishidan@haizhi.com>
2018-03-22 16:11:08 +0800

    update post_list

--- search.js
+++ search.js
@@ -2,88 +2,14 @@
 
     var G = window || this,
         even = G.BLOG.even,
-        $ = G.BLOG.$,
-        searchIco = $('#search'),
-        searchWrap = $('#search-wrap'),
-        keyInput = $('#key'),
-        back = $('#back'),
-        searchPanel = $('#search-panel'),
-        searchResult = $('#search-result'),
-        searchTpl = $('#search-tpl').innerHTML,
-        JSON_DATA = (G.BLOG.ROOT + '/content.json').replace(/\/{2}/g, '/'),
-        searchData;
-
-    function loadData(success) {
-
-        if (!searchData) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', JSON_DATA, true);
-
-            xhr.onload = function () {
-                if (this.status >= 200 && this.status < 300) {
-                    var res = JSON.parse(this.response);
-                    searchData = res instanceof Array ? res : res.posts;
-                    success(searchData);
-                } else {
-                    console.error(this.statusText);
-                }
-            };
-
-            xhr.onerror = function () {
-                console.error(this.statusText);
-            };
-
-            xhr.send();
-
-        } else {
-            success(searchData);
-        }
-    }
-
-    function tpl(html, data) {
-        return html.replace(/\{\w+\}/g, function (str) {
-            var prop = str.replace(/\{|\}/g, '');
-            return data[prop] || '';
-        });
-    }
+        select = G.BLOG.$,
+        searchIco = select('#search'),
+        searchWrap = select('#search-wrap'),
+        keyInput = select('#key'),
+        back = select('#back');
 
     var noop = G.BLOG.noop;
-    var root = $('html');
-
-    var Control = {
-        show: function () {
-            G.innerWidth < 760 ? root.classList.add('lock-size') : noop;
-            searchPanel.classList.add('in');
-        },
-        hide: function () {
-            G.innerWidth < 760 ? root.classList.remove('lock-size') : noop;
-            searchPanel.classList.remove('in');
-        }
-    };
-
-    function render(data) {
-        var html = '';
-        if (data.length) {
-
-            html = data.map(function (post) {
-
-                return tpl(searchTpl, {
-                    title: post.title,
-                    path: (G.BLOG.ROOT + '/' + post.path).replace(/\/{2,}/g, '/'),
-                    date: new Date(post.date).toLocaleDateString(),
-                    tags: post.tags.map(function (tag) {
-                        return '<span>#' + tag.name + '</span>';
-                    }).join('')
-                });
-
-            }).join('');
-
-        } else {
-            html = '<li class="tips"><i class="icon icon-coffee icon-3x"></i><p>Results not found!</p></li>';
-        }
-
-        searchResult.innerHTML = html;
-    }
+    var root = select('html');
 
     function regtest(raw, regExp) {
         regExp.lastIndex = 0;
@@ -102,18 +28,6 @@
             return;
         }
 
-        var regExp = new RegExp(key.replace(/[ ]/g, '|'), 'gmi');
-
-        loadData(function (data) {
-
-            var result = data.filter(function (post) {
-                return matcher(post, regExp);
-            });
-
-            render(result);
-            Control.show();
-        });
-
         e.preventDefault();
     }
 
@@ -126,12 +40,10 @@
 
     back.addEventListener(even, function () {
         searchWrap.classList.remove('in');
-        Control.hide();
     });
 
     document.addEventListener(even, function (e) {
         if (e.target.id !== 'key' && even === 'click') {
-            Control.hide();
         }
     });
 
